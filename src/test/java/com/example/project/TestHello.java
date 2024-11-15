64da5fe6b57376df565f9eb25ee4df3b63ff0075
SV-CSBSJU <160878675+SV-CSBSJU@users.noreply.github.com>
2024-11-15 15:56:30 -0600

    Update TestHello.java

--- src/test/java/com/example/project/TestHello.java
+++ src/test/java/com/example/project/TestHello.java
@@ -1,30 +1,13 @@
-package com.example.project;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
-
+import junit.framework.TestCase;
+import org.junit.Test;
+import java.util.*;
 import java.io.*;
-
-public class TestHello {
-
-   @Test
-   public void testHelloWorld()
-   {
-     PrintStream originalOut = System.out;
-     ByteArrayOutputStream bos = new ByteArrayOutputStream();
-     System.setOut(new PrintStream(bos));
-
-     // action
-     Hello.main(null);
-
-     // assertion
-     assertEquals("Hello world!\n", bos.toString());
-
-     // undo the binding in System
-     System.setOut(originalOut);
-   }
+public class TestHello extends TestCase{
+  
+  TestHello testLab = new TestHello();
+  @Test
+  public void testHello()throws FileNotFoundException{
+    assertEquals("Checking testhello ", "Hello World!\n", TestHello.testHello(coin));
+    
+  } 
 }
